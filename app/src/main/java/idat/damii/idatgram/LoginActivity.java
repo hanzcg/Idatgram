5d8a06d562120e1ffa80263b641c4949f51684b3
Hanz Cocchi Guerrero <hanzcg@hotmail.com>
2024-04-13 16:26:45 -0500

    Adding Login
    Initial login with splash screen

--- app/src/main/java/idat/damii/idatgram/LoginActivity.java
+++ app/src/main/java/idat/damii/idatgram/LoginActivity.java
@@ -8,13 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import idat.damii.idatgram.databinding.ActivityLoginBinding;
 
+public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Idatgram);
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        //setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
